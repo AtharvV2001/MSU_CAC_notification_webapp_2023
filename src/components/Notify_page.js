cbc8b4455b1d75229a0469e425c6986749a1a2ec
Dhruv <dhruvvara3@gmail.com>
2023-03-03 15:39:12 +0530

    ui made

---
+++ src/components/Notify_page.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Notification from "./Notification";
+
+const Notify_page = () => {
+    return (
+        <main>
+            <div class="title">
+                <h2>Notification</h2>
+            </div>
+            <div class="new">
+                <h3>New</h3>
+            </div>
+            <Notification />
+            
+        </main>
+    )
+}
+
+export default Notify_page;
