9760b13b6710c9e3253398bf422bbe0dc0ec9b2e
Atharv Vikhe <atharvvikhe@gmail.com>
2023-03-03 16:24:28 +0530

    Added fb to UI

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,28 +1,37 @@
 import React from "react";
 
-const Notification = () => {
+const Notification = ({
+    id,
+    userName,
+    date,
+    userImg,
+    postImg,
+    text,
+    isRead,
+    type,
+}) => {
     return (
         <div className="tnotification1">
-                <div className="user1">
-                    <div className="userprofile1">
-                        <img src="image/image.jpg" alt="" width="55px" />
-                    </div>
-                    <div className="usertext">
-                        <div className="username1">
-                            <span>
-                                <b>
-                                    viraj.dhimmar
-                                </b>
-                            </span>
-                            liked your profile. 24h
-                        </div>
-                    </div>
+            <div className="user1">
+                <div className="userprofile1">
+                    <img src={userImg} alt="" width="55px" />
                 </div>
-
-                <div className="post1">
-                    <img src="image/image.jpg" alt="" width="55px" />
+                <div className="usertext">
+                    <div className="username1">
+                        <span>
+                            <b>
+                                {userName}
+                            </b>
+                        </span>
+                        {" "}{text} 24h
+                    </div>
                 </div>
             </div>
+
+            <div className="post1">
+                <img src={postImg} alt="" width="55px" />
+            </div>
+        </div>
     );
 }
 
--- src/components/Notify_page.js
+++ src/components/Notify_page.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Notification from "./Notification";
+import { collection, getDocs, Timestamp } from "firebase/firestore";
+import { useEffect } from 'react';
+import { firebaseDB } from "../firebase/firebase";
 
 const Notify_page = () => {
+    const [notifyList, setNotifyList] = useState([]);
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const querySnapshot = await getDocs(collection(firebaseDB, "notification"));
+        const list = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+        setNotifyList(list);
+    }
+
     return (
         <main>
             <div className="title">
@@ -10,8 +28,19 @@ const Notify_page = () => {
             <div className="new">
                 <h3>New</h3>
             </div>
-            <Notification />
-            
+            {notifyList.map(notify => (
+                <Notification
+                    key={notify.id}
+                    id={notify.id}
+                    userName={notify.notifyFrom}
+                    date={notify.notifyDateTime.toDate()}
+                    userImg={notify.notifyUserImage}
+                    postImg={notify.notifyPostImage}
+                    text={notify.notifyText}
+                    isRead={notify.readFlag}
+                    type={notify.notifyType}
+                />
+            ))}
         </main>
     )
 }
